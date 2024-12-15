b5ade116baab3ee1e068055ce28bb728ab1cbaab
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2024-12-15 06:11:28 +0000

    Use utils functions

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -76,76 +76,6 @@ def parse_args() -> argparse.Namespace:
     logger.info(f"{args=}")
     return args
 
-@functools.cache
-def get_datacube_dir() -> pathlib.Path:
-    for p in get_data_root().iterdir():
-        if p.is_dir() and p.name.startswith('dynamicrouting_datacube'):
-            path = p
-            break
-    else:
-        for p in get_data_root().iterdir():
-            if any(pattern in p.name for pattern in ('session_table', 'nwb', 'consolidated', )):
-                path = get_data_root()
-                break
-        else:
-            raise FileNotFoundError(f"Cannot determine datacube dir: {list(get_data_root().iterdir())=}")
-    logger.info(f"Using files in {path}")
-    return path
-
-@functools.cache
-def get_data_root(as_str: bool = False) -> pathlib.Path:
-    expected_paths = ('/data', '/tmp/data', )
-    for p in expected_paths:
-        if (data_root := pathlib.Path(p)).exists():
-            logger.info(f"Using {data_root=}")
-        return data_root.as_posix() if as_str else data_root
-    else:
-        raise FileNotFoundError(f"data dir not present at any of {expected_paths=}")
-
-@functools.cache
-def get_nwb_paths() -> tuple[pathlib.Path, ...]:
-    return tuple(get_data_root().rglob('*.nwb'))
-    
-
-def get_nwb(session_id_or_path: str | pathlib.Path, raise_on_missing: bool = True, raise_on_bad_file: bool = True) -> pynwb.NWBFile:
-    if isinstance(session_id_or_path, (pathlib.Path, upath.UPath)):
-        nwb_path = session_id_or_path
-    else:
-        if not isinstance(session_id_or_path, str):
-            raise TypeError(f"Input should be a session ID (str) or path to an NWB file (str/Path), got: {session_id_or_path!r}")
-        if pathlib.Path(session_id_or_path).exists():
-            nwb_path = session_id_or_path
-        elif session_id_or_path.endswith(".nwb") and any(p.name == session_id_or_path for p in get_nwb_paths()):
-            nwb_path = next(p for p in get_nwb_paths() if p.name == session_id_or_path)
-        else:
-            try:
-                nwb_path = next(p for p in get_nwb_paths() if p.stem == session_id_or_path)
-            except StopIteration:
-                msg = f"Could not find NWB file for {session_id_or_path!r}"
-                if not raise_on_missing:
-                    logger.error(msg)
-                    return
-                else:
-                    raise FileNotFoundError(f"{msg}. Available files: {[p.name for p in get_nwb_paths()]}") from None
-    logger.info(f"Reading {nwb_path}")
-    try:
-        nwb = pynwb.NWBHDF5IO(nwb_path).read()
-    except RecursionError:
-        msg = f"{nwb_path.name} cannot be read due to RecursionError (hdf5 may still be accessible)"
-        if not raise_on_bad_file:
-            logger.error(msg)
-            return
-        else:
-            raise RecursionError(msg)
-    else:
-        return nwb
-
-def ensure_nonempty_results_dir() -> None:
-    # pipeline can crash if a results folder is expected and not found, and requires creating manually:
-    results = pathlib.Path("/results")
-    results.mkdir(exist_ok=True)
-    if not list(results.iterdir()):
-        (results / uuid.uuid4().hex).touch()
 
 # processing function ---------------------------------------------- #
 # modify the body of this function, but keep the same signature
@@ -164,7 +94,7 @@ def process_session(session_id: str, params: "Params", test: int = 0, skip_exist
     # Choose how to handle these as appropriate for your capsule
 
     try:
-        session = get_nwb(session_id, raise_on_missing=True, raise_on_bad_file=True) 
+        session = utils.get_nwb(session_id, raise_on_missing=True, raise_on_bad_file=True) 
     except (FileNotFoundError, RecursionError) as exc:
         logger.info(f"Skipping {session_id}: {exc!r}")
         return
@@ -354,19 +284,23 @@ def main():
     
     # if session_id is passed as a command line argument, we will only process that session,
     # otherwise we process all session IDs that match filtering criteria:    
-    session_table = pd.read_parquet(get_datacube_dir() / 'session_table.parquet')
+    session_table = pd.read_parquet(utils.get_datacube_dir() / 'session_table.parquet')
     session_table['issues']=session_table['issues'].astype(str)
     session_ids: list[str] = session_table.query(args.session_table_query)['session_id'].values.tolist()
     logger.debug(f"Found {len(session_ids)} session_ids available for use after filtering")
+    
     if args.session_id is not None:
         if args.session_id not in session_ids:
             logger.warning(f"{args.session_id!r} not in filtered session_ids: exiting")
             exit()
         logger.info(f"Using single session_id {args.session_id} provided via command line argument")
         session_ids = [args.session_id]
+    elif utils.is_pipeline(): 
+        # only one nwb will be available 
+        session_ids = set(session_ids) & set(p.stem for p in utils.get_nwb_paths())
     else:
         logger.info(f"Using list of {len(session_ids)} session_ids after filtering")
-
+    
     # run processing function for each session, with test mode implemented:
     for session_id in session_ids:
         try:
@@ -379,7 +313,7 @@ def main():
         if args.test:
             logger.info("Test mode: exiting after first session")
             break
-    ensure_nonempty_results_dir()
+    utils.ensure_nonempty_results_dir()
     logger.info(f"Time elapsed: {time.time() - t0:.2f} s")
 
 if __name__ == "__main__":
